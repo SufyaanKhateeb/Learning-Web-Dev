643f44b3deb4804b59cd2c5c61a6fcb4bc3034e4
Sufyaan Khateeb <sufyaankhateeb@gmail.com>
2021-06-15 18:17:02 +0530

    updated day5

--- Frontend/Day 5/calc.js
+++ Frontend/Day 5/calc.js
@@ -1,25 +1,44 @@
-var ans='';
+var eq='';
 function dis(val)
 {
+    var op = document.getElementById("input").value;
+    var ch = op.charAt(op.length-1);
+    if(val=='-'||val=='+'||val=='×'||val=='÷')
+    {
+        if(ch=='-'||ch=='+'||ch=='×'||ch=='÷'||ch=='')
+            return;
+    }
+    if(val=='.')
+    {
+        if(ch=='.')
+            return;
+    }
     document.getElementById("input").value+=val;
     if(val=='÷')
         val='/';
     if(val=='×')
         val='*';
-    ans+=val;
+    eq+=val;
 }
   
+function del() {
+    var del = document.getElementById("input").value;
+    document.getElementById("input").value=del.substring(0,del.length - 1);
+    eq=eq.substring(0,eq.length - 1);
+}
+
 //function that evaluates the digit and return result
 function solve()
 {
-    let x = ans;
+    let x = eq;
     let y = eval(x);
     document.getElementById("input").value = y;
+    eq=y;
 }
   
 //function that clear the display
 function clr()
 {
     document.getElementById("input").value = "";
-    ans='';
+    eq='';
 }
