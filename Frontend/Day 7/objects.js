df5933c6c2bd4a5d36f877cc712280ac1ef9a077
Sufyaan Khateeb <sufyaankhateeb@gmail.com>
2021-06-15 22:11:51 +0530

    day 7

--- Frontend/Day 7/objects.js
+++ Frontend/Day 7/objects.js
@@ -32,9 +32,9 @@
 // console.log(volume(obj));
 
 //6
-var library = [ { title: 'The Road Ahead', author: 'Bill Gates', libraryID: 1254 }, { title: 'Walter Isaacson', author: 'Steve Jobs', libraryID: 4264 }, { title: 'Mockingjay: The Final Book of The Hunger Games', author: 'Suzanne Collins', libraryID: 3245 }];
+// var library = [ { title: 'The Road Ahead', author: 'Bill Gates', libraryID: 1254 }, { title: 'Walter Isaacson', author: 'Steve Jobs', libraryID: 4264 }, { title: 'Mockingjay: The Final Book of The Hunger Games', author: 'Suzanne Collins', libraryID: 3245 }];
 
-library.sort((a,b) => 
-    b.libraryID - a.libraryID
-);
-console.log(library);
+// library.sort((a,b) => 
+//     b.libraryID - a.libraryID
+// );
+// console.log(library);
