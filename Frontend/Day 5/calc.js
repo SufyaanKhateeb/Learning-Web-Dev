521a55e3304c02e16d646efd5ec4ffd5d1a153db
Sufyaan Khateeb <sufyaankhateeb@gmail.com>
2021-06-16 17:18:08 +0530

    upadated day 5 js

--- Frontend/Day 5/calc.js
+++ Frontend/Day 5/calc.js
@@ -1,27 +1,37 @@
 var eq='';
+var flag1=0;
 function dis(val)
 {
     var op = document.getElementById("input").value;
     var ch = op.charAt(op.length-1);
     if(val=='-'||val=='+'||val=='×'||val=='÷')
     {
+        if(flag1==1)
+            flag1=0;
         if(ch=='-'||ch=='+'||ch=='×'||ch=='÷'||ch=='')
             return;
     }
     if(val=='.')
     {
-        if(ch=='.')
+        if(ch=='.' || flag1==1)
             return;
+        flag2=0;
+        flag1=1;
+        flag2=flag1;
     }
     document.getElementById("input").value+=val;
 }
   
 function del() {
     var del = document.getElementById("input").value;
+    var dot = del.charAt(del.length-1);
+    if(dot=='.')
+        flag1=0;
+    if((dot=='-'||dot=='+'||dot=='×'||dot=='÷') && flag2==1)
+        flag1=1;
     document.getElementById("input").value=del.substring(0,del.length - 1);
 }
 
-//function that evaluates the digit and return result
 function solve()
 {
     eq=document.getElementById("input").value;
@@ -33,9 +43,10 @@ function solve()
     eq=y;
 }
   
-//function that clear the display
 function clr()
 {
     document.getElementById("input").value = "";
     eq='';
+    flag1=0;
+    flag2=0;
 }
