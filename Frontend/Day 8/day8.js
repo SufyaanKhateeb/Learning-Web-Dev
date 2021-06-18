c7741bfd93f6aa724e887821951609ae8958dfed
Sufyaan Khateeb <sufyaankhateeb@gmail.com>
2021-06-18 11:51:56 +0530

    day 8

---
+++ Frontend/Day 8/day8.js
@@ -0,0 +1,15 @@
+var box = document.querySelector(".gridbox");
+
+for(let i = 0; i < 400; i++) {
+  box.innerHTML += '<div class="box" onclick="changecolor(this)"'+(i+1)+' ></div>';
+} 
+
+const changecolor=(item)=> item.classList.toggle("changecolor");
+
+const pixels = document.querySelectorAll('.box');
+const toPaint = [128, 129, 132, 133, 147, 150, 151, 154, 166, 175, 186, 195, 206, 215, 227, 234, 248, 253, 269, 272, 290, 291];
+pixels.forEach((p, i) => {
+	if (toPaint.includes(i + 1)) {
+		p.classList.toggle("changecolor");
+	}
+});
