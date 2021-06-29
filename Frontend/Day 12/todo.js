9570ffb98ae4d534be53adc08dcd00dd37cc1eff
Sufyaan Khateeb <sufyaankhateeb@gmail.com>
2021-06-30 01:27:07 +0530

    day 12

---
+++ Frontend/Day 12/todo.js
@@ -0,0 +1,59 @@
+const inputBox = document.querySelector(".inputField input");
+const addBtn = document.querySelector(".inputField button");
+const todoList = document.querySelector(".todoList");
+
+inputBox.onkeyup = ()=>{
+    let userEnteredValue = inputBox.value;
+    if(userEnteredValue.trim() != 0){
+        addBtn.classList.add("active");
+    }else{
+        addBtn.classList.remove("active");
+    }
+}
+showTasks();
+addBtn.onclick = ()=>{
+    let userEnteredValue = inputBox.value; 
+    let getLocalStorageData = localStorage.getItem("todolist");
+    if(getLocalStorageData == null){
+        list = [];
+    }else{
+        list = JSON.parse(getLocalStorageData);
+    }
+    list.push(userEnteredValue);
+    localStorage.setItem("todolist", JSON.stringify(list));
+    showTasks();
+    addBtn.classList.remove("active");
+}
+function showTasks(){
+    let getLocalStorageData = localStorage.getItem("todolist");
+    
+    if(getLocalStorageData == null){
+        list = [];
+    }else{
+        list = JSON.parse(getLocalStorageData); 
+    }
+
+    todoList.innerHTML='';
+    list.forEach((element, index) => {
+        let li = document.createElement('li');
+        li.innerHTML=element;
+        let span = document.createElement('span');
+        span.className='icon';
+        // span.onclick=deleteTask(index);
+        span.setAttribute('onclick', 'deleteTask('+index+')');
+        let i = document.createElement('i');
+        i.className='fas fa-trash';
+        span.appendChild(i);
+        li.appendChild(span);
+        todoList.appendChild(li);
+    });
+    inputBox.value = ""; 
+}
+
+function deleteTask(index){
+    let getLocalStorageData = localStorage.getItem("todolist");
+    list = JSON.parse(getLocalStorageData);
+    list.splice(index, 1);
+    localStorage.setItem("todolist", JSON.stringify(list));
+    showTasks();
+}
