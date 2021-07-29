58ef1a2bad1667cfec394f3baa7c9e364fd2cfc1
Sufyaan Khateeb <sufyaankhateeb@gmail.com>
2021-07-29 22:09:51 +0530

    day 24

--- Frontend/Day 24/navigation/src/Navbar.jsx
+++ Frontend/Day 24/navigation/src/Navbar.jsx
@@ -1,67 +1,73 @@
-import React,{useContext} from "react";
+import React, { useContext } from "react";
 import {
   BrowserRouter as Router,
   Switch,
   Route,
   Link,
-  Redirect
+  Redirect,
 } from "react-router-dom";
-import "./index.css"
-import Context,{AuthContext} from './context';
+import "./index.css";
+import Context, { AuthContext } from "./context";
 
 export default function NavBar() {
   return (
     <Router>
       <div>
         <div className="navbar">
-            <Link className="link" to="/">Home</Link>
-            <Link className="link" to="/about">About</Link>
-            <Link className="link" to="/profile">Profile</Link>
-            <Link className="link" to="/dashboard">Dashboard</Link>
+          <Link className="link" to="/">
+            Home
+          </Link>
+          <Link className="link" to="/about">
+            About
+          </Link>
+          <Link className="link" to="/profile">
+            Profile
+          </Link>
+          <Link className="link" to="/dashboard">
+            Dashboard
+          </Link>
         </div>
 
-        <hr />
-        
         <Switch>
-        <AuthContext>
-          <Route exact path="/">
-            <Home />
-          </Route>
-          <Route path="/about">
-            <About />
-          </Route>
-           <Route path="/profile">
-            <Profile />
-          </Route>
-          <Route path="/dashboard">
-            <Dashboard />
-          </Route>
-        </AuthContext>
+          <AuthContext>
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route path="/about">
+              <About />
+            </Route>
+            <Route path="/profile">
+              <Profile />
+            </Route>
+            <Route path="/dashboard">
+              <Dashboard />
+            </Route>
+          </AuthContext>
         </Switch>
       </div>
     </Router>
   );
 }
 
-
 function Home() {
-
   const logger = useContext(Context);
-    return (
+  return (
     <div>
       <h1>Home</h1>
       <div>
         <h2>If not logged in can't access, Profile & Dashboard</h2>
       </div>
-      {
-        logger.loading?
-          logger.loggedIn?
-            "Loading...":
-            "Loading...":
-          logger.loggedIn?
-            <button onClick={() => logger.logout()}>Logout</button> :
-            <button onClick={() => logger.login()}>Login</button>
-      }
+      {logger.loading ? (
+        logger.loggedIn ? (
+          "Loading..."
+        ) : (
+          "Loading..."
+        )
+      ) : logger.loggedIn ? (
+        <button onClick={() => logger.logout()}>Logout</button>
+      ) : (
+        <button onClick={() => logger.login()}>Login</button>
+      )}
     </div>
   );
 }
@@ -74,27 +80,23 @@ function About() {
   );
 }
 
-function Profile(){
-    const logger = useContext(Context)
+function Profile() {
+  const logger = useContext(Context);
   const loggedIn = logger.loggedIn;
   return (
     <Route>
       <h1>Profile</h1>
-      {
-        loggedIn ? "": <Redirect to="/"/>
-      }
+      {loggedIn ? "" : <Redirect to="/" />}
     </Route>
-);
+  );
 }
 
 function Dashboard() {
-  const logger = useContext(Context)
+  const logger = useContext(Context);
   return (
     <Route>
       <h1>Dashboard</h1>
-      {
-        logger.loggedIn ? "" : <Redirect to="/"/>
-      }
+      {logger.loggedIn ? "" : <Redirect to="/" />}
     </Route>
   );
 }
